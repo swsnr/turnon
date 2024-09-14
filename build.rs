2415a6c8bee45d1c2704c651b16945f5cd745ab5
Sebastian Wiesner <sebastian@swsnr.de>
2024-09-14 13:27:55 +0200

    Compile blueprint files in build.rs

--- build.rs
+++ build.rs
@@ -4,7 +4,37 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+use std::path::PathBuf;
+
+/// Compile all blueprint files.
+fn compile_blueprint() {
+    let blueprint_files: Vec<PathBuf> = glob::glob("resources/**/*.blp")
+        .unwrap()
+        .collect::<Result<_, _>>()
+        .unwrap();
+    for blueprint_file in &blueprint_files {
+        println!("cargo:rerun-if-changed={}", blueprint_file.display());
+    }
+
+    let output = std::process::Command::new("blueprint-compiler")
+        .args(["batch-compile", "resources", "resources"])
+        .args(&blueprint_files)
+        .output()
+        .unwrap();
+
+    assert!(
+        output.status.success(),
+        "blueprint-compiler failed with exit status {} and stderr:\n{}",
+        output.status,
+        String::from_utf8_lossy(&output.stderr)
+    );
+}
+
 fn main() {
+    // Since blueprint generates UI files for glib resources, we must compile
+    // blueprint files before compiling resources!
+    compile_blueprint();
+
     glib_build_tools::compile_resources(
         &["resources"],
         "resources/resources.gresource.xml",
