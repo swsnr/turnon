eaa426b579c91f3b3e881a0acdd75dd72f278ca8
Sebastian Wiesner <sebastian@swsnr.de>
2024-12-18 21:43:09 +0100

    Disable settings portal for screenshots

--- screenshots/run-for-screenshot.bash
+++ screenshots/run-for-screenshot.bash
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
-exec env G_MESSAGES_DEBUG=all GSETTINGS_BACKEND=memory cargo run -- \
+
+set -euo pipefail
+
+variables=(
+    # Run app with default settings: Force the in-memory gsettings backend to
+    # block access to standard Gtk settings, and tell Adwaita not to access
+    # portals to prevent it from getting dark mode and accent color from desktop
+    # settings.
+    #
+    # Effectively this makes our app run with default settings.
+    GSETTINGS_BACKEND=memory
+    ADW_DISABLE_PORTAL=1
+)
+
+exec env "${variables[@]}" cargo run -- \
     --devices-file "$(git rev-parse --show-toplevel)/screenshots/devices.json"
