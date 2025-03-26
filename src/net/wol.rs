8df7bab0fc90beb20f9ec0e4241d2148de616701
Sebastian Wiesner <sebastian@swsnr.de>
2025-03-26 18:57:40 +0100

    Use my wol package to fill magic packet

--- src/net/wol.rs
+++ src/net/wol.rs
@@ -6,7 +6,6 @@
 
 //! Wake On LAN (magic packet) implementation.
 
-use std::io::Write;
 use std::net::{Ipv4Addr, SocketAddr};
 
 use glib::IOCondition;
@@ -14,15 +13,6 @@ use gtk::gio::Cancellable;
 use gtk::gio::prelude::{SocketExt, SocketExtManual};
 use gtk::gio::{self, IOErrorEnum};
 use macaddr::MacAddr6;
-
-/// Write a magic packet for the given `mac_address` to `sink`.
-fn write_magic_packet<W: Write>(sink: &mut W, mac_address: MacAddr6) -> std::io::Result<()> {
-    sink.write_all(&[0xff; 6])?;
-    for _ in 0..16 {
-        sink.write_all(mac_address.as_bytes())?;
-    }
-    Ok(())
-}
 
 /// Send a magic Wake On LAN packet to the given `mac_address`.
 ///
@@ -45,7 +35,7 @@ pub async fn wol(mac_address: MacAddr6) -> Result<(), glib::Error> {
         ));
     }
     let mut payload = [0; 102];
-    write_magic_packet(&mut payload.as_mut_slice(), mac_address).unwrap();
+    wol::fill_magic_packet(&mut payload, mac_address);
     let broadcast_and_discard_address: gio::InetSocketAddress =
         SocketAddr::new(Ipv4Addr::BROADCAST.into(), 9).into();
     let bytes_sent = socket.send_to(
@@ -53,40 +43,6 @@ pub async fn wol(mac_address: MacAddr6) -> Result<(), glib::Error> {
         payload,
         Cancellable::NONE,
     )?;
-    assert!(bytes_sent == 102);
+    assert!(bytes_sent == payload.len());
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use macaddr::MacAddr6;
-
-    use super::write_magic_packet;
-
-    #[test]
-    fn test_write_magic_packet() {
-        let mac_address = "26:CE:55:A5:C2:33".parse::<MacAddr6>().unwrap();
-        let mut buffer = Vec::new();
-        write_magic_packet(&mut buffer, mac_address).unwrap();
-        let expected_packet: [u8; 102] = [
-            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // // Six all 1 bytes
-            0x26, 0xCE, 0x55, 0xA5, 0xC2, 0x33, // 16 repetitions of the mac address
-            0x26, 0xCE, 0x55, 0xA5, 0xC2, 0x33, //
-            0x26, 0xCE, 0x55, 0xA5, 0xC2, 0x33, //
-            0x26, 0xCE, 0x55, 0xA5, 0xC2, 0x33, //
-            0x26, 0xCE, 0x55, 0xA5, 0xC2, 0x33, //
-            0x26, 0xCE, 0x55, 0xA5, 0xC2, 0x33, //
-            0x26, 0xCE, 0x55, 0xA5, 0xC2, 0x33, //
-            0x26, 0xCE, 0x55, 0xA5, 0xC2, 0x33, //
-            0x26, 0xCE, 0x55, 0xA5, 0xC2, 0x33, //
-            0x26, 0xCE, 0x55, 0xA5, 0xC2, 0x33, //
-            0x26, 0xCE, 0x55, 0xA5, 0xC2, 0x33, //
-            0x26, 0xCE, 0x55, 0xA5, 0xC2, 0x33, //
-            0x26, 0xCE, 0x55, 0xA5, 0xC2, 0x33, //
-            0x26, 0xCE, 0x55, 0xA5, 0xC2, 0x33, //
-            0x26, 0xCE, 0x55, 0xA5, 0xC2, 0x33, //
-            0x26, 0xCE, 0x55, 0xA5, 0xC2, 0x33, //
-        ];
-        assert_eq!(buffer.as_slice(), expected_packet.as_slice());
-    }
 }
